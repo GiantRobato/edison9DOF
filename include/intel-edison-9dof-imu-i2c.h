9318b6999a73edb85dc2eaf19e819cf0ba2b6a06
penguindustin <penguindustin@gmail.com>
2015-04-02 16:48:02 +0000

    fixed multiple type definitions error

--- include/intel-edison-9dof-imu-i2c.h
+++ include/intel-edison-9dof-imu-i2c.h
@@ -1,6 +1,8 @@
 #ifndef __INTEL_EDISON_9DOF_IMU_I2C_H__
 #define __INTEL_EDISON_9DOF_IMU_I2C_H__
 
+#include <stdint.h>
+
 //The i2c bus being used
 #define I2C_DEV_NAME		"/dev/i2c-1"
 
@@ -13,7 +15,7 @@
 #define WHO_AM_I_G			0x0F
 
 int DMOpenDevFile();
-int DMReadI2CMessage(int devFile, uint8_t i2cAddress, uint8_t regAddress, uint8_t *returnData, uint8_t count);
+int DMReadI2CMessages(int devFile, uint8_t i2cAddress, uint8_t regAddress, uint8_t *returnData, uint8_t count);
 int DMReadI2CMessage(int devFile, uint8_t i2cAddress, uint8_t regAddress, uint8_t *returnData);
 
 #endif
